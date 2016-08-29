759e630a14b78eba75eb86fa92e7589d96a96ea3
arknotts <arknotts@gmail.com>
2016-08-29 16:35:41 -0400

    Add getDisplayValue method to SerializableFilter
    This is optional as not all filters may want to have their values added as a tag.

--- source/filters/filter.ts
+++ source/filters/filter.ts
@@ -1,4 +1,4 @@
-import * as Rx from 'rxjs/Rx';
+import { Subscription, Observable, BehaviorSubject } from 'rxjs/Rx';
 
 import { objectUtility } from '../services/object/object.service';
 
@@ -9,7 +9,9 @@ export interface IFilterWithCounts extends IFilter {
 export interface ISerializableFilter<TFilterData> extends IFilter {
 	type: string;
 	serialize(): TFilterData;
-	subscribe(onValueChange: IValueChangeCallback<TFilterData>): Rx.Subscription;
+	subscribe(onValueChange: IValueChangeCallback<TFilterData>): Subscription;
+	asObservable(): Observable<TFilterData>;
+	getDisplayValue?: { (value: TFilterData): string };
 }
 
 export interface IValueChangeCallback<TFilterData> {
@@ -22,11 +24,11 @@ export interface IFilter {
 
 export abstract class SerializableFilter<TFilterData> implements ISerializableFilter<TFilterData> {
 	type: string;
-	protected subject: Rx.Subject<TFilterData>;
+	protected subject: BehaviorSubject<TFilterData>;
 	private _value: TFilterData;
 
 	constructor() {
-		this.subject = new Rx.Subject<TFilterData>();
+		this.subject = new BehaviorSubject<TFilterData>(null);
 	}
 
 	abstract filter(item: any): boolean;
@@ -35,8 +37,12 @@ export abstract class SerializableFilter<TFilterData> implements ISerializableFi
 		return <any>this;
 	}
 
-	subscribe(onValueChange: IValueChangeCallback<TFilterData>): Rx.Subscription {
+	subscribe(onValueChange: IValueChangeCallback<TFilterData>): Subscription {
 		return this.subject.subscribe(onValueChange);
+	}
+
+	asObservable(): Observable<TFilterData> {
+		return this.subject.asObservable();
 	}
 
 	onChange(force: boolean = true): void {
